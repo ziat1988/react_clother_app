01abbe15c42c56d352440e90599b1c4b0b5bf61e
ziat1988 <trinhdangnhat@gmail.com>
2020-05-09 11:23:56 +0200

    template general

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './directory.style.scss';
+import MenuItem from '../menu-item/menu-item.component'
+import sections from './data.js'
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: sections
+
+        }
+    }
+
+
+    render() {
+        return (
+
+            <div className='directory-menu'>
+
+                {this.state.sections.map(({title,imageUrl,id,size}) =>
+                        <MenuItem key= {id} title={title} imageUrl={imageUrl} size ={size}/>
+                )}
+
+            </div>
+        )
+    }
+
+}
+
+export default Directory;
